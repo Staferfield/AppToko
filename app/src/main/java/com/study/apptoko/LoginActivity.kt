59466baa649f7920c396e27ba2e7bd38806a248d
Stevi Saputro <stevisaputro@gmail.com>
2022-01-03 19:06:46 +0700

    PCS 9 Pt.3 - Penerapan Shared Preferences untuk menyimpan status login

--- app/src/main/java/com/study/apptoko/LoginActivity.kt
+++ app/src/main/java/com/study/apptoko/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.study.apptoko
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,11 +10,18 @@ import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.study.apptoko.api.BaseRetrofit
 import com.study.apptoko.response.login.LoginResponse
+import com.study.apptoko.utils.SessionManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
+    companion object{
+        // lateinit = lazy, tetapi memakai var sehingga isi variabel dapat dirubah
+        lateinit var sessionManager: SessionManager
+        private lateinit var context: Context
+    }
+
     // inisialisasi variabel
     // sama dengan private val api = BaseRetrofit().endpoint, tetapi by lazy lebih optimal untuk memory karena hanya akan di inisialisasi saat digunakan
     private val api by lazy { BaseRetrofit().endpoint }
@@ -22,6 +31,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        sessionManager = SessionManager(this)
+
+        // Cek status login, apbila sudah login, langung pindah ke MainActivity
+        val loginStatus = sessionManager.getBoolean("LOGIN_STATUS")
+        if(loginStatus){
+            val moveIntent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(moveIntent)
+            finish()
+        }
+
         // Deklarasi komponen sesuai UI di Layout
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         val txtEmail = findViewById<TextInputEditText>(R.id.txtEmail)
@@ -29,8 +48,6 @@ class LoginActivity : AppCompatActivity() {
 
         // Buat listener dan tindakan saat button di klik
         btnLogin.setOnClickListener {
-            Toast.makeText(this, "Login Proses", Toast.LENGTH_SHORT).show()
-
             // Jalankan API login
             api.login(txtEmail.text.toString(), txtPassword.text.toString()).enqueue(
                 object :
@@ -41,6 +58,23 @@ class LoginActivity : AppCompatActivity() {
                         response: Response<LoginResponse>
                     ) {
                         Log.e("LoginData",response.toString())
+                        // Cek dari respon data success
+                        val correct = response.body()!!.success
+
+                        // Apabila success -> true
+                        if(correct){
+                            val token = response.body()!!.data.token
+                            // Simpan data token ke Shared Preferences
+                            sessionManager.saveString("TOKEN", "Bearer "+token)
+                            sessionManager.saveBoolean("LOGIN_STATUS", true)
+
+                            // Pindah ke MainActivity
+                            val moveIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(moveIntent)
+                            finish()
+                        }else{ // Apabila success -> false
+                            Toast.makeText(applicationContext, "User dan password salah", Toast.LENGTH_LONG).show()
+                        }
                     }
                     // Apabila error
                     override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
@@ -49,7 +83,5 @@ class LoginActivity : AppCompatActivity() {
                 }
             )
         }
-
-
     }
 }
