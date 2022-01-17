f8e5dfcfa9b503f5511f0378601f13b14103b0fd
Stevi Saputro <stevisaputro@gmail.com>
2022-01-17 19:01:56 +0700

    PCS PTM 13 Pt.1 - Melengkapi Codingan Module Transaksi

--- app/src/main/java/com/study/apptoko/adapter/TransaksiAdapter.kt
+++ app/src/main/java/com/study/apptoko/adapter/TransaksiAdapter.kt
@@ -6,10 +6,19 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.study.apptoko.CallbackInterface
 import com.study.apptoko.R
+import com.study.apptoko.response.cart.Cart
 import com.study.apptoko.response.produk.Produk
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TransaksiAdapter (val listProduk: List<Produk>): RecyclerView.Adapter<TransaksiAdapter.ViewHolder>() {
+
+    var callbackInterface: CallbackInterface? = null
+    var total: Int = 0
+    var cart: ArrayList<Cart> = arrayListOf<Cart>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransaksiAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_transaksi, parent, false)
@@ -19,24 +28,60 @@ class TransaksiAdapter (val listProduk: List<Produk>): RecyclerView.Adapter<Tran
     override fun onBindViewHolder(holder: TransaksiAdapter.ViewHolder, position: Int) {
         val produk = listProduk[position]
         holder.txtNamaProduk.text = produk.nama
-        holder.txtHarga.text = produk.harga
+
+        // Format ke rupiah
+        val localeID =  Locale("in", "ID")
+        val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+
+        holder.txtHarga.text = numberFormat.format(produk.harga.toDouble()).toString()
 
         holder.btnPlus.setOnClickListener {
             val old_value = holder.txtQty.text.toString().toInt()
             val new_value = old_value+1
 
             holder.txtQty.setText(new_value.toString())
+            // Rubah nilai Total
+            total = total + produk.harga.toString().toInt()
+
+            // Cek apabila sudah ada data di cart, kemudian hapus
+            val index = cart.indexOfFirst { it.id == produk.id.toInt() }.toInt()
+            if(index!=-1){
+                cart.removeAt(index)
+            }
+            // Tambahkan data cart baru
+            val itemCart = Cart(produk.id.toInt(),produk.harga.toInt(),new_value)
+            cart.add(itemCart)
+
+            // Kirim data total bayar dam cart baru
+            callbackInterface?.passResultCallback(total.toString(),cart)
         }
 
         holder.btnMinus.setOnClickListener {
             val old_value = holder.txtQty.text.toString().toInt()
             val new_value = old_value-1
 
-            // Hanya kurangi kalau nilai lebih dari nol
-            if (new_value>=0){
-                holder.txtQty.setText(new_value.toString())
+            // Cek apabila sudah ada data di cart, kemudian hapus
+            val index = cart.indexOfFirst { it.id == produk.id.toInt() }.toInt()
+
+            if(index!=-1){
+                cart.removeAt(index)
             }
 
+            // Biarkan data terhapus apabila jumlah nol
+            if (new_value>=0){
+                holder.txtQty.setText(new_value.toString())
+
+                total = total - produk.harga.toString().toInt()
+            }
+            // Hanya kurangi kalau nilai lebih dari satu
+            if (new_value>=1){
+                // Tambahkan data cart baru
+                val itemCart = Cart(produk.id.toInt(),produk.harga.toInt(),new_value)
+                cart.add(itemCart)
+            }
+
+            // Kirim data total bayar baru
+            callbackInterface?.passResultCallback(total.toString(),cart)
         }
 
     }
